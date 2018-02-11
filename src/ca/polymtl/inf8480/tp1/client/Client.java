c90f325b08791060e1d48cd72985125a4a5fc08e
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-02-10 19:51:21 -0500

    Parsing de la commande

--- src/ca/polymtl/inf8480/tp1/client/Client.java
+++ src/ca/polymtl/inf8480/tp1/client/Client.java
@@ -12,26 +12,44 @@ public class Client {
 
 	public static void main(String[] args) {
 		String distantHostname = null;
-		/*Le deuxieme argument sert a aire des test de l'impact de la longueur sur le temps d'appel*/
 
-		if (args.length > 0)
+		Client client = new Client(distantHostname);
+		if( args.length == 0 )
 		{
-			distantHostname = args[0];
+			client.runTests();
 		}
 		else
 		{
-			System.out.print("You need to use one of the following parameters :\n-\tlist\n-\tcreate\n-\tlock\n-\tget\n-\tsyncLocalDirectory\n");
-			return;
+			client.parseArgs(args);
+			client.runCmd();
 		}
+	}
 
-		Client client = new Client(distantHostname);
-		client.runTests();
+	private enum Command {CREATE, LIST, GET, SYNC, LOCK, PUSH}
 
+	public void runCmd(){
+		try {
+			switch(command){
+				case CREATE:
+					serverStub.create(argument);
+					break;
+				default:
+					System.out.println("Command " + commandStr + " not yet implemented");
+					System.exit(1);
+					break;
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private ServerInterface distantServerStub = null;
 	private ServerInterface localServerStub = null;
+	private ServerInterface serverStub = null;
 	private static final boolean USE_DISTANT_SERVER = false;
+	private Command command = null;
+	private String commandStr = null;
+	private String argument = null;
 
 	public Client(String distantServerHostname) {
 		super();
@@ -40,9 +58,11 @@ public class Client {
 				distantServerStub = loadServerStub(distantServerHostname);
 				System.out.println("called loadServerStub with hostname " + distantServerHostname);
 			}
+			serverStub = distantServerStub;
+		} else {
+			localServerStub = loadServerStub("127.0.0.1");
+			serverStub = localServerStub;
 		}
-
-		localServerStub = loadServerStub("127.0.0.1");
 	}
 
 	public void runTests() {
@@ -76,5 +96,54 @@ public class Client {
 		}
 
 		return stub;
+	}
+
+	public void parseArgs(String[] args){
+		commandStr = args[0];
+
+		if( commandStr.equals("create") ){
+			command = Command.CREATE;
+		} else if( commandStr.equals("list") ){
+			command = Command.LIST;
+		} else if (commandStr.equals("get") ){
+			command = Command.GET;
+		} else if (commandStr.equals("syndLocalDirectory") ){
+			command = Command.LIST;
+		} else if (commandStr.equals("lock") ){
+			command = Command.LIST;
+		} else if (commandStr.equals("push") ){
+			command = Command.LIST;
+		} else {
+			System.out.print("You need to use one of the following parameters :"
+					+ "\n-\tlist\n-\tcreate\n-\tlock\n-\tget\n-\tsyncLocalDirectory\n");
+			System.exit(1);
+		}
+
+		if(args.length > 1)
+			argument = args[1];
+		validateArgs();
+	}
+
+	private void validateArgs(){
+		// Check if command takes arguments
+		switch(command){
+			case CREATE:
+			case GET:
+			case PUSH:
+			case LOCK:
+				if( argument == null ){
+					System.out.println("Command " + commandStr + " requires an argument");
+					System.exit(1);
+				}
+				break;
+			case LIST:
+			case SYNC:
+				if( argument != null ){
+					System.out.println("Command " + commandStr + " does not take any arguments");
+					System.exit(1);
+				}
+				break;
+		}
+
 	}
 }
